f3ff0bcbcd5db40ec03d46b207e8eba835a9dd5c
Parami1 <musiconmix@gmail.com>
2024-03-15 20:24:17 +0700

    true

--- src/main/java/com/example/training01/controller/login/login.java
+++ src/main/java/com/example/training01/controller/login/login.java
@@ -1,29 +1,63 @@
 package com.example.training01.controller.login;
 
+import com.example.training01.model.UserLogin;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class login {
 
     @GetMapping("/login")
-    String loginPage() {
+    String loginPage(ModelMap model) {
+
+        UserLogin ulog = new UserLogin();
+
+        model.addAttribute("userlogin", ulog);
+
         return "login";
     }
 
     @GetMapping("/test")
     String showTestPage() {
-        return "testindex";
+        return "testpage";
     }
 
-    @GetMapping("/header")
-    String he(){
-//      101  th:fragment="header"
-        return "fragment/header";
+    @GetMapping("/lay")
+    String showLayout() {
+        return "layout/layout";
     }
 
-    @GetMapping("/he2")
-    String he2(){
-        return "fragment/navigation";
+    @GetMapping("/home")
+    String showHeome() {
+        return "home";
     }
+
+    @GetMapping("/l1")
+    String showload() {
+        return "loading";
+    }
+
+    @GetMapping("/l2")
+    String showload2() {
+        return "loading2";
+    }
+
+    @PostMapping("/login")
+    String submitForm2(@ModelAttribute ModelMap model) {
+
+        System.out.println(model);
+
+        if (model.getAttribute("inputUsername") == null) {
+
+            model.addAttribute("userp", "No user.");
+
+            return "login";
+        }
+
+        return "home";
+    }
+
 }
---
+++ src/main/java/com/example/training01/model/UserLogin.java
@@ -0,0 +1,10 @@
+package com.example.training01.model;
+
+import lombok.Data;
+
+@Data
+public class UserLogin {
+
+    private String user;
+    private String pass;
+}
